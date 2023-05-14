a8ee9e5e2c7bcdf9fc73c5c305c93de60c6a9340
mohitsvpi <singhalmohit011@gmail.com>
2023-05-14 18:36:38 +0530

    Added Error handling

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,17 +1,17 @@
-const User = require('../models/user.model');
+const User = require("../models/user.model");
+const asyncHandler = require("express-async-handler");
 
-const createUser = async (req, res) => {
-    const {email} = req.body;
-    const findUser = await User.findOne({email: email});
-    if(!findUser) {
-        const newUser = await User.create(req.body);
-        return res.status(201).send(newUser);
-    }
-    else {
-        return res.send({ message: 'User already exists'});
-    }
-}
+const createUser = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const findUser = await User.findOne({ email: email });
+  if (!findUser) {
+    const newUser = await User.create(req.body);
+    return res.status(201).send(newUser);
+  } else {
+   throw new Error('User already exists');
+  }
+});
 
 module.exports = {
-    createUser
-}
+  createUser,
+};
